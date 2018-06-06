896d756cfa96c7e0c868c9337eb57016e6a91376
lcaslax <lca@slax.it>
2018-06-06 10:43:07 +0200

    HACK: Eliminata mail programmatore nell'intestazione py

--- cl_gsmat.py
+++ cl_gsmat.py
@@ -17,7 +17,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-# e-mail:flavio.giovannangeli@gmail.com
+# e-mail:
 
 
 #   M O D U L E S  #
